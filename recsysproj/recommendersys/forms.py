d1c802045b681391d912a177b65888788e1e2c76
deadania <deadania14@gmail.com>
2019-07-18 08:40:38 +0700

    last changes

--- recsysproj/recommendersys/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Reviews
-
-class ReviewsForm(forms.ModelForm):
-    class Meta:
-        model = Reviews
-        fields = ('item_set', 'user_set', 'testing_set')
